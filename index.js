9659538d2138e76234de67b966189665286735b9
firestormDev <79257881+firestormDev@users.noreply.github.com>
2021-12-12 22:51:49 -0300

    atualizações

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const async = require('hbs/lib/async');
-
 let http = require ('http'),
     path = require ('path'),
     express = require ('express'),
-    app = express(),
-    Posts = require('./model/Posts');  
+    app = express();
+
     app.set('view engine', 'hbs');
     app.set('views', path.join(__dirname, 'view'));
     app.use(express.static(path.join(__dirname, 'public')));
     app.get(express.urlencoded({extended: false}));
-    
 
     app.get('/home', (req, res) => {
         res.render('home', { title: 'Página inicial'});
@@ -20,24 +17,6 @@ let http = require ('http'),
     });
     app.get('/cadastro', (req, res) => {
         res.render('cadastro', { title: 'Página de cadastro'});
-    });
-    app.get('/', (req, res) => {
-        res.render('index');
-    });
-
-    app.get('/mensagem_get', (req, res) => {
-        let msg = req.query.mensagem;
-        console.log(msg);
-        res.end();
-    });
-
-    app.get('/posts', async (req, res) => {
-        /*const posts =  [
-            {content: 'post 1'},
-            {content: 'post 2'}
-        ];**/
-        const posts = await Posts.find();        
-        res.render('posts', { posts: posts});
     });
 
     app.listen(3000);
