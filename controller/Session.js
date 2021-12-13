b0ac9fb3eff04882f608705b54895c9427a8463d
firestormDev <79257881+firestormDev@users.noreply.github.com>
2021-12-12 23:23:28 -0300

    Add files via upload

---
+++ controller/Session.js
@@ -0,0 +1,111 @@
+isLogado();
+
+
+function isLogado(){
+   var token = localStorage.getItem('token');
+
+   buttonEntrar = document.getElementById('entrarButton');
+
+   if(token === "QpwL5tke4Pnpja7X4"){
+      console.log("Você já está logado!")
+      document.getElementById("loginButton").style.display = "none";     
+      document.getElementById("sairButton").style.display = "display";
+      buttonEntrar.style.display = "none";
+      return true;
+   
+   }
+   else{
+      console.log("Você não está logado!");
+      document.getElementById("loginButton").style.display = "block";      
+      document.getElementById("sairButton").style.display = "none";
+      document.getElementById("buscarButton").style.display = "none";
+      buttonEntrar.style.display = "block";
+      return false;
+   }
+}
+
+
+/*Modal-Entrar*/
+
+function iniciaModal(modalID) {
+    const modal = document.getElementById(modalID);    
+     modal.classList.add('mostrar');
+     modal.addEventListener('click', function (e) {
+        if (e.target.id == modalID || e.target.className == 'fechar' )
+        modal.classList.remove('mostrar');            
+
+});
+
+}
+
+ buttonEntrar = document.getElementById('entrarButton');
+ buttonEntrar.addEventListener('click', function () {
+     iniciaModal('modal-entrar');
+ })
+
+ /*buttonBusca = document.getElementById('buscarButton');
+ buttonBusca.addEventListener('click', function () {
+     iniciaModal4('modal-busca');
+   })*/
+
+ buttonSair = document.getElementById('sairButton');
+ buttonSair.addEventListener('click', function () {
+     deslogar();
+
+     // recarregar pagina 
+     document.location.reload(true);
+ })
+
+
+
+
+document.getElementById("loginButton2").addEventListener('click', function(){
+   logar()
+});
+
+
+
+
+function logar(){
+   var email = document.getElementById("email_login").value;
+   var pass = document.getElementById("senha_login").value;
+
+   loginApi(email, pass);
+
+}
+
+function deslogar(){
+   localStorage.removeItem('token');
+
+   buttonEntrar = document.getElementById('entrarButton');
+   buttonSair = document.getElementById('sairButton');
+
+   buttonEntrar.style.display = "block";
+   buttonSair.style.display = "none";
+
+}
+
+/*função para LocalStorage*/
+
+function store(token) {
+    localStorage.setItem('token', token);
+ 
+ }
+ /*função para validação*/
+
+function check() {
+
+    /* storage cadastro */
+    var storedEmail = localStorage.getItem('email');
+    var storedPw = localStorage.getItem('senha');
+ 
+    /* storage login */
+    var userEmail = document.getElementById('email').value;
+    var userPw = document.getElementById('senha').value;
+ 
+    if(userEmail.value == storedEmail && userPw.value == storedPw) {
+       console.log('Você já está logado.');
+    }else {
+       console.log('ERROR.');
+    }
+ }
